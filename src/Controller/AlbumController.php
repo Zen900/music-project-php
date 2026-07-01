c7fbc1e387c20d37e64ddf31d70623f2e5845840
Zen900 <162557475+Zen900@users.noreply.github.com>
2026-07-01 12:01:34 +0200

    show Album

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Album;
-use App\Repository\AlbumRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,16 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AlbumController extends AbstractController
 {
     #[Route('/album/{id}', name: 'album_show')]
-    public function show(AlbumRepository $albumRepository, int $id): Response
+    public function show(Album $album): Response
     {
-        $album = $albumRepository
-            ->find($id);
 
-                    if (!$album) {
-            return new Response('Album not found', 404);
-        }
-
-        // ...
-        return new Response('Check out this great product: '.$album->getAlbumTitle());
+        return $this->render('albums.html.twig', [
+            'album' => $album,
+        ]);
     }
 }
